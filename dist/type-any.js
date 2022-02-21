136358d4ce1639ba99bf42c407025fb320909055
mitre88 <bedr10_capacitacion@hotmail.com>
2022-02-20 18:45:37 -0600

    "domingo por la tarde"

--- dist/type-any.js
+++ dist/type-any.js
@@ -1,4 +1,27 @@
 "use strict";
-//tipo de dato explicito:
-let idUser;
-idUser = 23;
+//enum:
+// enum año{
+//     enero,
+//     febrero,
+//     marzo,
+//     abril,
+//     mayo,
+// };
+// enum restoAño{
+//     junio,
+//     julio,
+//     agosto,
+//     septiembre,
+//     octubre,
+// };
+// let todoAño={...año,...restoAño,...{noviembre:`noviembre`},...{diciembre:`diciembre`}};
+// console.table(todoAño);
+//creaciónn de arreglo:
+let letra = {
+    nombre1: `alejandro`,
+    nombre2: `juan`,
+    nombre3: `pedro`,
+    nombre4: `maria`,
+    nombre5: `jose`, //y un objeto tiene propiedades que tienen un valor; 
+};
+console.log(letra);
--- type-any.ts
+++ type-any.ts
@@ -1,7 +1,31 @@
-//tipo de dato explicito:
+//enum:
+// enum año{
+//     enero,
+//     febrero,
+//     marzo,
+//     abril,
+//     mayo,
+// };
+// enum restoAño{
+//     junio,
+//     julio,
+//     agosto,
+//     septiembre,
+//     octubre,
+// };
+// let todoAño={...año,...restoAño,...{noviembre:`noviembre`},...{diciembre:`diciembre`}};
+// console.table(todoAño);
 
-let idUser:any ;
-idUser=23;
+//creaciónn de arreglo:
+let letra={   //existe diferencia porque un array es un conjunto ordenado de elementos;
+    nombre1:`alejandro`,
+    nombre2:`juan`,
+    nombre3:`pedro`,
+    nombre4:`maria`,
+    nombre5:`jose`, //y un objeto tiene propiedades que tienen un valor; 
+}
+console.log(letra);
+
 
 
 
